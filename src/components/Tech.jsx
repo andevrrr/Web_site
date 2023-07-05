89285a6e290d232eebea21ba8e770722d3d9f8e2
Anton <kucherenko-anton1@mail.ru>
2023-07-05 12:33:15 +0300

    added react useEffect

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { BallCanvas } from './canvas'
 import { SectionWrapper } from '../hoc'
 
--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Tilt from 'react-parallax-tilt';
 import { motion } from 'framer-motion';
 
