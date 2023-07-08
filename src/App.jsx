cf1236c0cd286b3b9d6f571d3301bfeb9327be70
Anton <kucherenko-anton1@mail.ru>
2023-07-08 22:22:38 +0300

    lil changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,6 @@
 import { BrowserRouter } from "react-router-dom"
 
-import Amplify from "aws-amplify";
-import awsconfig from "./aws-exports";
 
-Amplify.configure(awsconfig);
 
 import { About, Contact, Hero, Navbar, Tech, Works, Footer } from './components';
 
