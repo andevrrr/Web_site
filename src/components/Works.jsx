f7b2907173fe2cf7d3882c6a0a76de7416deb376
Anton <kucherenko-anton1@mail.ru>
2023-09-09 23:08:24 +0300

    changed to schema

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -46,7 +46,7 @@ const ProjectCard = ({ index, name, description, tags, image, source_code_link }
         <h3 className="text-white font-bold text-[24px]">{name}</h3>
         <p className="mt-2 text-secondary text-[14px]">{description}</p>
         <div className="mt-4 flex flex-wrap gap-2">
-          {tags.items.map((tag) => (
+          {tags.map((tag) => (
             <p key={tag.name}
               className={`text-[14px] ${tag.color}`}>
               #{tag.name}
