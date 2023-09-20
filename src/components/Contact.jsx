bbb74a77c0d6f58e39326bc90cc5f2974ff51693
Anton <kucherenko-anton1@mail.ru>
2023-09-20 10:40:08 +0300

    ContactForm from formspree added

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -3,67 +3,13 @@ import { motion } from "framer-motion";
 import { styles } from "../styles";
 import { SectionWrapper } from "../hoc";
 import { slideIn } from "../utils/motion";
-import { API, graphqlOperation } from "aws-amplify";
-import { createContactSubmission } from "../graphql/mutations";
+
 
 const Contact = () => {
-  const formRef = useRef();
-  const [form, setForm] = useState({
-    name: "", 
-    email: "",
-    message: "",
-  });
-
-  const [loading, setLoading] = useState(false);
-
-  const handleChange = (e) => {
-    const { target } = e;
-    const { name, value } = target;
-
-    setForm({
-      ...form,
-      [name]: value,
-    });
-  };
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    setLoading(true);
-    const { name, email, message } = form; // Define the 'message' variable
-  
-    if (name && email && message) { // Check that all required fields are not empty
-      try {
-        await API.graphql({
-          query: createContactSubmission,
-          variables: {
-            input: {
-              name,
-              email,
-              message, // Include the 'message' variable here
-            },
-          },
-        });
-  
-        setLoading(false);
-        alert("Thank you. I will get back to you as soon as possible.");
-  
-        setForm({
-          name: "",
-          email: "",
-          message: "",
-        });
-      } catch (error) {
-        setLoading(false);
-        console.error(error);
-  
-        alert("Ahh, something went wrong. Please try again.");
-      }
-    } else {
-      setLoading(false);
-      alert("Please fill in all required fields.");
-    }
-  };
-  
+  const [state, handleSubmit] = useForm("xgejnyzy");
+  if (state.succeeded) {
+    return <p>Thanks for the message, I will reply to you shortly!</p>;
+  }
 
   return (
     <div
@@ -76,55 +22,13 @@ const Contact = () => {
         <p className={styles.sectionSubText}>Get in touch</p>
         <h3 className={styles.sectionHeadText}>Contact.</h3>
 
-        <form
-          ref={formRef}
-          onSubmit={handleSubmit}
-          className="mt-12 flex flex-col gap-8"
-        >
-          <label className="flex flex-col">
-            <span className="text-white font-medium mb-4">Your Name</span>
-            <input
-              type="text"
-              name="name"
-              value={form.name}
-              onChange={handleChange}
-              placeholder="What's your good name?"
-              className="bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium"
-            />
-          </label>
-          <label className="flex flex-col">
-            <span className="text-white font-medium mb-4">Your email</span>
-            <input
-              type="email"
-              name="email"
-              value={form.email}
-              onChange={handleChange}
-              placeholder="What's your web address?"
-              className="bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium"
-            />
-          </label>
-          <label className="flex flex-col">
-            <span className="text-white font-medium mb-4">Your Message</span>
-            <textarea
-              rows={7}
-              name="message"
-              value={form.message}
-              onChange={handleChange}
-              placeholder="What you want to say?"
-              className="bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium"
-            />
-          </label>
+        {/* Replace the existing form with the ContactForm component */}
+        <ContactForm />
 
-          <button
-            type="submit"
-            className="bg-tertiary py-3 px-8 rounded-xl outline-none w-fit text-white font-bold shadow-md shadow-primary"
-          >
-            {loading ? "Sending..." : "Send"}
-          </button>
-        </form>
       </motion.div>
     </div>
   );
 };
+
 
 export default SectionWrapper(Contact, "contact");
