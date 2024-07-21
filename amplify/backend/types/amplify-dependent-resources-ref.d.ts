9aff60e3894ff45f03ccf5a9431af16600a39949
Anton Kucherenko <anton.kucherenko@tuni.fi>
2024-07-21 16:44:19 +0200

    amplify delete & init

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,6 +1,6 @@
 export type AmplifyDependentResourcesAttributes = {
   "api": {
-    "portfolio": {
+    "personal": {
       "GraphQLAPIEndpointOutput": "string",
       "GraphQLAPIIdOutput": "string",
       "GraphQLAPIKeyOutput": "string"
